577b051b39d5cf90ddd98cd7b8ba742b43efa360
throne <fahthrone@gmail.com>
2021-07-13 18:33:59 +0800

    project init

---
+++ dgit.py
@@ -0,0 +1,4 @@
+import libdgit
+
+
+libdgit.main()
---
+++ libdgit.py
@@ -0,0 +1,12 @@
+import argparse
+import collections
+import configparser
+import hashlib
+import os
+import re
+import sys
+import zlib
+
+
+def main():
+    print("ok!")
